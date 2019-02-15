93eaf0dc64d73fab29ac73f04fbf5ab3eb60104d
Leonard Meagher <LeonardMeagher2@gmail.com>
2019-02-15 15:30:12 -0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,23 +1,51 @@
-function getBlogPosts(){
-  return fetch("https://api.github.com/repos/leonardmeagher2/leonardmeagher2.github.io/contents/blog")
-  .then(function(response){
-    return response.json();
-  })
+function basename(path) {
+  return path.replace(/^.*\/|\.[^.]*$/g, "");
 }
 
-function basename(path){
-  return path.replace(/^.*\/|\.[^.]*$/g, '');
-}
 function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function(word) {
-    return (word.charAt(0).toUpperCase() + word.slice(1));
-  }).join(' ');
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 }
 
-function parsePostName(post){
-  var parts = post.name.split("-")
-  return {
-    date:parts[0].replace(/_/g,"/"),
-    name:titleCase(basename(parts[1]).replace(/_/g," "))
-  }
+function getBlogPosts() {
+  return fetch(
+    "https://api.github.com/repos/leonardmeagher2/leonardmeagher2.github.io/contents/blog"
+  ).then(function(response) {
+    return response.json().map(function(post){
+      var parts = post.name.split("-");
+      return {
+        file_path: `/blog/${post.name}`,
+        title: titleCase(basename(parts[1]).replace(/_/g, " ")),
+        date: new Date(parts[0].replace(/_/g, "/"))
+      }
+    }).sort(function(a,b){
+      return a.date - b.date;
+    });
+  });
 }
+
+function showPost(post) {
+  fetch(post.file_name)
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(html) {
+    // move this to templates
+    var section = document.createElement("section");
+    var header = document.createElement("h2");
+    header.textContent = post.title;
+    section.appendChild(header);
+    section.insertAdjacentHTML("beforeend", html);
+    document.body.appendChild(section);
+  });
+}
+
+
+getBlogPosts().then(function(posts){
+  showPost(posts[0]);
+});

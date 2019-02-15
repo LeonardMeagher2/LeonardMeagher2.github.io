8f000eb6f5d1a620917fc307b7392d9cedae1b7c
Leonard Meagher <LeonardMeagher2@gmail.com>
2019-02-15 10:26:06 -0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -5,10 +5,19 @@ function getBlogPosts(){
   })
 }
 
+function basename(path){
+  return path.replace(/^.*\/|\.[^.]*$/g, '');
+}
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
 function parsePostName(post){
   var parts = post.name.split("-")
   return {
     date:parts[0].replace(/_/g,"/"),
-    name:parts[1].replace(/_/g," ")
+    name:titleCase(basename(parts[1]).replace(/_/g," "))
   }
 }
